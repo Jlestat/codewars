95b69d1ebe1beeebd0534637d86539a7ea2a0065
Jlestat <jlestatt@gmail.com>
2023-04-28 22:48:17 +0300

    Beginner Series #3 Sum of Numbers

---
+++ Beginner Series #3 Sum of Numbers.py
@@ -0,0 +1,13 @@
+def get_sum(a: int,b: int) -> int:
+    fin_sum = 0
+    if a < b:
+        for i in range(a, b + 1):
+            fin_sum += i
+    else:
+        for i in range(b, a + 1):
+            fin_sum += i
+
+    return fin_sum
+
+
+print(get_sum(0, -1))
