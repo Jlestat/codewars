8d730d809b7255ca6714fab6f063c161bd1df54b
Jlestat <jlestatt@gmail.com>
2023-04-29 21:39:18 +0300

    Primes in numbers

---
+++ Primes in numbers.py
@@ -0,0 +1,26 @@
+def primeFactors(n: int) -> str:
+    i = 2
+    final_set = {}
+    while n / i != 1:
+        if n % i == 0:
+            if i in final_set:
+                final_set[i] = final_set[i] + 1
+            else:
+                final_set[i] = 1
+            n = n / i
+        else:
+            i += 1
+
+    if i in final_set:
+        final_set[i] = final_set[i] + 1
+    else:
+        final_set[i] = 1
+    final_str = ''
+    res = sorted(final_set.items(), key=lambda a: a[0])
+
+    for i in res:
+        if i[1] == 1:
+            final_str = final_str + '(' + str(i[0]) + ')'
+        else:
+            final_str = final_str + '(' + str(i[0]) + '**' + str(i[1]) + ')'
+    return final_str
