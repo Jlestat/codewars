5526e3a18debd7125db283bb5e991c11d2157b28
Jlestat <jlestatt@gmail.com>
2023-05-05 22:34:44 +0300

    Find the vowels

---
+++ Find the vowels.py
@@ -0,0 +1,11 @@
+def vowel_indices(word: str) -> list:
+    final_list = []
+    for i in range(len(word)):
+        if word[i] in 'aeiouyAEIOUY':
+            final_list.append(i + 1)
+        else:
+            continue
+    return final_list
+
+
+print(vowel_indices('123456'))
