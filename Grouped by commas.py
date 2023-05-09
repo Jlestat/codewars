06e0c15fd42c822418afa02156b81473e4cf972f
Jlestat <jlestatt@gmail.com>
2023-05-09 21:21:28 +0300

    Grouped by commas

---
+++ Grouped by commas.py
@@ -0,0 +1,12 @@
+def group_by_commas(n: int) -> str:
+    final_str = ''
+    n = str(n)[::-1]
+    for i in range(len(str(n))- 1, -1, -1):
+        if i % 3 == 0:
+            final_str += str(n)[i] + ','
+        else:
+            final_str += str(n)[i]
+    return final_str[:-1]
+
+
+print(group_by_commas(35235235))
