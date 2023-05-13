23a335edb5aeb405b905de9513f7f46763c9a470
Jlestat <jlestatt@gmail.com>
2023-05-13 22:56:57 +0300

    first character that repeats

---
+++ first character that repeats.py
@@ -0,0 +1,18 @@
+def first_dup(s: str) -> str:
+    break_out_flag = False
+    final_str = ''
+    for i in range(len(s)):
+        for j in range(i + 1, len(s)):
+            if s[i] == s[j]:
+                final_str += s[i]
+                break_out_flag =True
+                break
+        if break_out_flag:
+            break
+    if final_str:
+        return final_str
+    else:
+        return None
+
+
+print(first_dup('1a2b3a3c'))
