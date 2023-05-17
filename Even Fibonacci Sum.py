9f19683c4662ef1e6365c6061e538c30d7bf5715
Jlestat <jlestatt@gmail.com>
2023-05-17 21:08:56 +0300

    Even Fibonacci Sum

---
+++ Even Fibonacci Sum.py
@@ -0,0 +1,12 @@
+def even_fib(m: int) -> int:
+    final_sum = 0
+    a = 0
+    b = 1
+    while a < m:
+        if a % 2 == 0:
+            final_sum += a
+        a, b = b, a + b
+    return final_sum
+
+
+print(even_fib(10))
