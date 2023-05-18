9f5fa39a263c9f7755072181a0ceee3f433990a4
Jlestat <jlestatt@gmail.com>
2023-05-18 23:22:17 +0300

    Arabian String

--- Arabian String.py
+++ Arabian String.py
@@ -2,6 +2,10 @@ from re import split
 
 
 def camelize(str_: str) -> str:
+    """
+    :param str_: str
+    :return: str
+    """
     return ''.join(i.capitalize() for i in split('([^a-zA-Z0-9])', str_) if i.isalnum())
 
 
