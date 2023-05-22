abec444e6d96a8533ed4490744bd1e892942e4bb
Jlestat <jlestatt@gmail.com>
2023-05-22 18:47:36 +0300

    Selective Array Reversing

---
+++ Selective Array Reversing.py
@@ -0,0 +1,15 @@
+def sel_reverse(arr: list,l: int) -> list:
+    final_list = []
+    if l == 0:
+        return arr
+    else:
+        for i in range(0, len(arr), l):
+            new_list = arr[i: i + l][::-1]
+            for j in new_list:
+                final_list.append(j)
+
+
+        return final_list
+
+
+print(sel_reverse([2,4,6,8,10,12,14,16], 3))
