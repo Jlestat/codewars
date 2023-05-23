557c8476160ce8af5f6b7bab40f35331ac5a587b
Jlestat <jlestatt@gmail.com>
2023-05-23 19:53:41 +0300

    Three added Characters

--- Three added Characters.py
+++ Three added Characters.py
@@ -1,4 +1,9 @@
 def added_char(s1: str, s2: str) -> str:
+    """
+    :param s1: str
+    :param s2: str
+    :return: str
+    """
     for i in s2:
         if s2.count(i) > s1.count(i):
             return i
