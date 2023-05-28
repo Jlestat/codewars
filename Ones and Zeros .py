be1a8a517b94df23a792572d3c63daa085d91e18
Jlestat <jlestatt@gmail.com>
2023-05-28 14:30:40 +0300

    Ones and Zeros

---
+++ Ones and Zeros .py
@@ -0,0 +1,11 @@
+def binary_array_to_number(arr: list) -> int:
+    final_sum = 0
+    n = 0
+    for i in range(len(arr)-1, -1, -1):
+        if arr[i] != 0:
+            final_sum += arr[i] * 2 ** n
+        n += 1
+    return final_sum
+
+
+print(binary_array_to_number([1,1,1,1]))
