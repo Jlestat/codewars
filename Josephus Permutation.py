74a728be1e4b48652bbe0d45fda562aaa09b45b5
Jlestat <jlestatt@gmail.com>
2023-05-29 22:53:00 +0300

    Josephus Permutation

---
+++ Josephus Permutation.py
@@ -0,0 +1,13 @@
+def josephus(items: list, k: int) -> list:
+    curr_list = items
+    final_list = []
+    i = 0
+    while len(curr_list) > 0:
+        j = (i + k - 1) % len(curr_list)
+        final_list.append(curr_list[j])
+        del curr_list[j]
+        i = j if j < len(curr_list) else 0
+    return final_list
+
+
+print(josephus([1,2,3,4,5,6,7,8,9,10],2))
