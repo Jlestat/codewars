50c6dd7c01023093c9c6aef636a023d9bc812f94
Jlestat <jlestatt@gmail.com>
2023-05-31 23:26:42 +0300

    Predict your age!

--- Predict your age!.py
+++ Predict your age!.py
@@ -1,6 +1,18 @@
 import math
 
 def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
+    """
+
+    :param age_1:
+    :param age_2:
+    :param age_3:
+    :param age_4:
+    :param age_5:
+    :param age_6:
+    :param age_7:
+    :param age_8:
+    :return:
+    """
     curr_list = [age_1, age_2, age_3, age_4,age_5, age_6, age_7, age_8]
     final_sum = 0
     for i in curr_list:
