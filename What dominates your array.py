7c8366c26bf47df664a3d403a57ddbd4ea789336
Jlestat <jlestatt@gmail.com>
2023-06-26 17:50:37 +0300

    dominator

---
+++ What dominates your array.py
@@ -0,0 +1,21 @@
+import collections
+
+def dominator(arr: list) -> int:
+    if len(arr) == 0:
+        return -1
+    elif len(arr) == 1:
+        return arr[0]
+    else:
+        n = collections.Counter(arr).most_common()
+        print(n)
+        if n[0][1] == n[1][1]:
+            return -1
+        else:
+            if arr.count(n[0][0]) > len(arr) // 2:
+                return n[0][0]
+            else:
+                return -1
+
+
+
+print(dominator([1]))
