39ee28ff2495c492de5453dc076b57334c281aa9
Jlestat <jlestatt@gmail.com>
2023-07-21 15:43:15 +0300

    Are we alternate?

---
+++ Are we alternate.py
@@ -0,0 +1,9 @@
+def is_alt(s: str) -> bool:
+    vowels = 'aeiou'
+    alph = 'bcdfghjklmnpqrstvwxyz'
+    for i, j in zip(s, s[1:]):
+        if i in vowels and j in vowels:
+            return False
+        if i in alph and j in alph:
+            return False
+    return True
