958afbb0ef2780acd86f4d9a147ce071a27f162d
Jlestat <jlestatt@gmail.com>
2023-10-06 22:47:34 +0300

    Simple beads count

--- Simple beads count.py
+++ Simple beads count.py
@@ -1,4 +1,8 @@
-def count_red_beads(n):
+def count_red_beads(n: int) -> int:
+    """
+    :param n: count of blue beads
+    :return: count of red beads
+    """
     if n < 2:
         return 0
     else:
