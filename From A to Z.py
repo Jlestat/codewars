34a0aab9aaf01f3c3bd85aae554051376cf0450f
Jlestat <jlestatt@gmail.com>
2023-10-09 19:32:34 +0300

    From A to Z

--- From A to Z.py
+++ From A to Z.py
@@ -1,4 +1,8 @@
-def gimme_the_letters(sp: str):
+def gimme_the_letters(sp: str) -> str:
+    """
+    :param sp: str
+    :return: str
+    """
     alph = 'abcdefghijklmnopqrstuvwxyz'
     if sp[0] == sp[0].upper():
         return alph[alph.index(sp[0].lower()):alph.index(sp[-1].lower()) + 1].upper()
