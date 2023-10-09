ed271de12ec2566b8f9a15905f1c63bd6d324f89
Jlestat <jlestatt@gmail.com>
2023-10-09 19:32:12 +0300

    From A to Z

---
+++ From A to Z.py
@@ -0,0 +1,9 @@
+def gimme_the_letters(sp: str):
+    alph = 'abcdefghijklmnopqrstuvwxyz'
+    if sp[0] == sp[0].upper():
+        return alph[alph.index(sp[0].lower()):alph.index(sp[-1].lower()) + 1].upper()
+    else:
+        return alph[alph.index(sp[0].lower()):alph.index(sp[-1].lower()) + 1]
+
+
+print(gimme_the_letters("Q-Z"))
