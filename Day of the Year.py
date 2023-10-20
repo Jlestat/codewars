f071758ba9e99c260fc43da6588d3857f263676b
Jlestat <jlestatt@gmail.com>
2023-10-20 22:23:08 +0300

    Day of the Year

---
+++ Day of the Year.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+
+def to_day_of_year(dat):
+    d = dat[0]
+    m = dat[1]
+    y = dat[2]
+    return (date(y, m, d) - date(y, 1, 1)).days + 1
+
+
+print(to_day_of_year([25, 12, 2017]))
