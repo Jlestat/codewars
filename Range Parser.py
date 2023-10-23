cd7cca772e711d62ab5a3cae4c1fd748de23bd9d
Jlestat <jlestatt@gmail.com>
2023-10-23 23:10:49 +0300

    Range Parser

---
+++ Range Parser.py
@@ -0,0 +1,21 @@
+import re
+
+
+def range_parser(s):
+    res = []
+    new_list = re.split(",|, ", s)
+    for i in new_list:
+        new_i = i.strip()
+        if '-' in new_i and ':' in new_i:
+            for j in range(int(new_i[:new_i.index('-')]), int(new_i[new_i.index('-') + 1:new_i.index(':')] ) + 1,
+                           int(new_i[new_i.index(':') + 1:])):
+                res.append(j)
+        elif '-' in new_i:
+            for j in range(int(new_i[:new_i.index('-')]), int(new_i[new_i.index('-') + 1:]) + 1):
+                res.append(j)
+        else:
+            res.append(int(i))
+    return res
+
+
+print(range_parser("1-10,14, 20-25:2"))
