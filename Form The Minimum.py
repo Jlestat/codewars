27a1f376fc193974b8e81b6baa87d7baf65da56e
Jlestat <jlestatt@gmail.com>
2023-10-31 21:51:14 +0300

    Form The Minimum

---
+++ Form The Minimum.py
@@ -0,0 +1,8 @@
+def min_value(digits):
+    set_dig = set(digits)
+    new_list = list(set_dig)
+    new_list.sort()
+    return int(''.join([str(i) for i in new_list]))
+
+
+print(min_value([1, 3, 1]))
