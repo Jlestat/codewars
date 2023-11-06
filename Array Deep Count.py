f5d0fe81a7f96f0df6314899498e1521180545f6
Jlestat <jlestatt@gmail.com>
2023-11-06 17:55:43 +0300

    Array Deep Count

---
+++ Array Deep Count.py
@@ -0,0 +1,9 @@
+from functools import reduce
+
+
+def deep_count(a):
+    if not isinstance(a, list):
+        return 0
+    if len(a) == 0:
+        return 0
+    return len(a) + reduce(lambda i, j : i + deep_count(j), a, 0)
