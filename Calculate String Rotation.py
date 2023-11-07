03e34487dd0465c5cb401a73685c8cfbfd3645d4
Jlestat <jlestatt@gmail.com>
2023-11-07 22:15:46 +0300

    Calculate String Rotation

---
+++ Calculate String Rotation.py
@@ -0,0 +1,10 @@
+def shifted_diff(str1, str2):
+    if len(str1) != len(str2):
+        return -1
+
+    for i in range(len(str1)):
+        if str1 == str2:
+            return i
+        str2 = str2[1:] + str2[0]
+
+    return -1
