54e6c1c06c84dfc4b3f6b9973fb220b83586236b
Jlestat <jlestatt@gmail.com>
2023-12-14 21:52:43 +0300

    Alphabet symmetry

---
+++ Alphabet symmetry.py
@@ -0,0 +1,15 @@
+def solve(strings : list[str]) -> list[int]:
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    res = []
+    for i in strings:
+        i = i.lower()
+        temp = 0
+        for j in range(len(i)):
+
+            if alpha.index(i[j]) == j:
+                temp += 1
+        res.append(temp)
+    return res
+
+
+print(solve(["abode","ABc","xyzD"]))
