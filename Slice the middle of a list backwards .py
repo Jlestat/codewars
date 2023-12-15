4ae3a0d8057fcd980ee720ed4c2d0e152b81261c
Jlestat <jlestatt@gmail.com>
2023-12-15 22:18:31 +0300

    Slice the middle of a list backwards

---
+++ Slice the middle of a list backwards .py
@@ -0,0 +1,8 @@
+def reverse_middle(lst: list):
+    if len(lst) % 2 == 0:
+        return lst[len(lst) // 2 - 1:len(lst) // 2 + 1][::-1]
+    else:
+        return lst[len(lst) // 2 - 1:len(lst) // 2 + 2][::-1]
+
+
+print(reverse_middle([4, 3, 100, 1, 10]))
