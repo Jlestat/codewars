6bddf4cfbcca012430c694f368270d7362d7fd4c
Jlestat <jlestatt@gmail.com>
2023-12-18 20:02:54 +0300

    More Zeros than Ones

---
+++ More Zeros than Ones.py
@@ -0,0 +1,7 @@
+def more_zeros(s):
+    res = []
+    for i in s:
+        ascii_code = ord(i)
+        if bin(ascii_code)[2:].count('0') > bin(ascii_code)[2:].count('1') and i not in res:
+            res.append(i)
+    return res
