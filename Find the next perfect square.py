c7e2391cc851b2739089ed902c21455cbacd4778
Jlestat <jlestatt@gmail.com>
2023-12-20 20:51:37 +0300

    Find the next perfect square!

--- Find the next perfect square.py
+++ Find the next perfect square.py
@@ -1,5 +1,9 @@
 import math
-def find_next_square(sq):
+def find_next_square(sq: int) -> int:
+    """
+    :param sq: int
+    :return: int
+    """
     if str(math.sqrt(sq))[-1] == '0':
         return int(math.sqrt(sq) + 1) ** 2
     else:
