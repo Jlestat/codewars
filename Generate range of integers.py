958c1f86559f586594fdc51209ae9afe1524643b
Jlestat <jlestatt@gmail.com>
2023-12-23 22:59:04 +0300

    Generate range of integers

--- Generate range of integers.py
+++ Generate range of integers.py
@@ -1,4 +1,11 @@
 def generate_range(start, stop, step):
+    """
+
+    :param start:
+    :param stop:
+    :param step:
+    :return:
+    """
     res = []
     for i in range(start, stop + 1, step):
         res.append(i)
