c94707611f08ea328a26099cec9ffb140a7739a9
Jlestat <jlestatt@gmail.com>
2023-12-30 23:53:13 +0300

    A Needle in the Haystack

--- Array Deep Count.py
+++ Array Deep Count.py
@@ -2,6 +2,11 @@ from functools import reduce
 
 
 def deep_count(a: list) -> int:
+    """
+
+    :param a:
+    :return:
+    """
     if not isinstance(a, list):
         return 0
     if len(a) == 0:
