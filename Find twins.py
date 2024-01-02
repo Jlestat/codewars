f2579e3a38e52ba80492f7fb57c38f4b43b8c94b
Jlestat <jlestatt@gmail.com>
2024-01-02 23:03:49 +0300

    Find twins

--- Find twins.py
+++ Find twins.py
@@ -1,4 +1,8 @@
-def elimination(arr: list):
+def elimination(arr: list) -> int:
+    """
+    :param arr: list
+    :return: int
+    """
     for i in arr:
         if arr.count(i) == 2:
             return i
