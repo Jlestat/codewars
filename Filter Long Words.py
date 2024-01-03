6bc8bb3bf8d066c79533aa1ef5a15476eb791f36
Jlestat <jlestatt@gmail.com>
2024-01-03 20:49:33 +0300

    Filter Long Words

--- Filter Long Words.py
+++ Filter Long Words.py
@@ -1,4 +1,4 @@
-def filter_long_words(sentence, n):
+def filter_long_words(sentence: list, n: int) -> list:
     curr_list = sentence.split()
     res = [i for i in curr_list if len(i) > n]
     return res
