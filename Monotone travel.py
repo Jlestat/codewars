b1be81fc5d8b89cd651409a7e0b289f018150d49
Jlestat <jlestatt@gmail.com>
2024-01-09 22:09:07 +0300

    Monotone travel

--- Monotone travel.py
+++ Monotone travel.py
@@ -1,4 +1,4 @@
-def is_monotone(heights: list):
+def is_monotone(heights: list) -> bool:
     curr_list = sorted(heights)
     str_list = [str(i) for i in curr_list]
     heights_str = [str(i) for i in heights]
