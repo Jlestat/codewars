f5f2614eb862b9b34e73ff7e4ad1532c54a14846
Jlestat <jlestatt@gmail.com>
2024-01-10 19:24:31 +0300

    Numbers to Letters

---
+++ Numbers to Letters.py
@@ -0,0 +1,9 @@
+def switcher(arr):
+    alpha = 'zyxwvutsrqponmlkjihgfedcba!? '
+    res = ''
+    for i in arr:
+        res += alpha[int(i) - 1]
+    return res
+
+
+print(switcher(['24', '12', '23', '22', '4', '26', '9', '8']))
