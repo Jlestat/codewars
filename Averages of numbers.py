57cfda027f24ffed25829a80764b89354d7affc7
Jlestat <jlestatt@gmail.com>
2024-01-14 14:32:06 +0300

    Averages of numbers

---
+++ Averages of numbers.py
@@ -0,0 +1,16 @@
+def averages(arr: list) -> list:
+    try:
+        res = []
+        for i in range(len(arr) - 1):
+
+            if i == len(arr):
+                res.append(arr[i] / 2)
+            else:
+                res.append((arr[i] + arr[i + 1]) / 2)
+
+        return res
+    except Exception:
+        return []
+
+
+print(averages([1, 3, 5, 1, -10]))
