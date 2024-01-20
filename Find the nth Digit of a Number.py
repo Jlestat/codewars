215e4e03a343ac0d7361f1aec1186dcb3a967043
Jlestat <jlestatt@gmail.com>
2024-01-20 21:07:48 +0300

    Find the nth Digit of a Number

---
+++ Find the nth Digit of a Number.py
@@ -0,0 +1,13 @@
+def find_digit(num, nth):
+    if nth <= 0:
+        return -1
+    else:
+        try:
+            if str(num)[::-1][nth - 1].isdigit():
+                return int(str(num)[::-1][nth - 1])
+            else:
+                return 0
+        except Exception:
+            return 0
+
+print(find_digit(-456, 4))
