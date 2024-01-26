0815dac5c7252efa8252e5d70abc0262addec4a5
Jlestat <jlestatt@gmail.com>
2024-01-26 23:36:00 +0300

    Replace all items

---
+++ Replace all items.py
@@ -0,0 +1,15 @@
+def replace_all(obj, find, rep):
+    if type(obj) == list:
+        res = []
+        for i in obj:
+            if i == find:
+                res.append(rep)
+            else:
+                res.append(i)
+        return res
+    else:
+        res = obj.replace(find, rep)
+        return res
+
+
+print(replace_all([1, 2, 3], 1, 2))
