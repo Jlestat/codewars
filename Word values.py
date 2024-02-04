6e2a197c4e940a743f41abfb4d581937c5312f6c
Jlestat <jlestatt@gmail.com>
2024-02-04 23:09:58 +0300

    Word values

---
+++ Word values.py
@@ -0,0 +1,17 @@
+def name_value(my_list):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    n = 0
+    result = []
+    for i in my_list:
+        curr_sum = 0
+        for j in i:
+            if j == ' ':
+                continue
+            else:
+                curr_sum += alpha.index(j) + 1
+        result.append(curr_sum * (n + 1))
+        n += 1
+    return result
+
+
+print(name_value(["abc abc","abc abc","abc","abc"]))
